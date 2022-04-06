a4c0e315f691baf3a9e32eabc83ba15960a0cce1
DAsm <zh.daur@gmail.com>
2022-04-06 08:57:11 +0600

    first commit

---
+++ bin/asar_server.dart
@@ -0,0 +1,5 @@
+import 'package:asar_server/asar_server.dart' as asar_server;
+
+void main(List<String> arguments) {
+  print('Hello world: ${asar_server.calculate()}!');
+}
---
+++ test/asar_server_test.dart
@@ -0,0 +1,8 @@
+import 'package:asar_server/asar_server.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
